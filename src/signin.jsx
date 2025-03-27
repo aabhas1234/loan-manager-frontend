6294b8442a87a60cb98e26af4690c8970f7a8efc
aabhas1234 <136249872+aabhas1234@users.noreply.github.com>
2025-03-27 09:09:48 +0530

    Update signin.jsx

--- src/signin.jsx
+++ src/signin.jsx
@@ -49,17 +49,18 @@ const signin = () => {
 
     return (
          <div className='bg-gray-200 p-3 text-center w-1/2 m-auto rounded-md my-3'>
+             <div className='font-bold text-black mx-auto w-fit'>WELCOME TO THE SIGNIN PAGE</div>
             <div className='m-6 text-left flex max-sm:flex-wrap'>
                 <label htmlFor='name ' className='mr-4 text-black font-bold' >Name:</label>
-                <input type='text' name='name' id='name' className='rounded-md max-sm:w-[7rem]' onChange={handler4}></input>
+                <input type='text' name='name' id='name' className='rounded-md max-sm:w-[7rem] bg-white' onChange={handler4}></input>
             </div>
             <div className='m-6 text-left w-fit flex max-sm:flex-wrap'>
                 <label htmlFor='email' className='mr-4 text-black font-bold'>Email:</label>
-                <input type='email' name='email' id='email' className='rounded-md max-sm:w-[7rem]' onChange={handler1}></input>
+                <input type='email' name='email' id='email' className='rounded-md max-sm:w-[7rem] bg-white' onChange={handler1}></input>
             </div>
             <div className='m-6 text-left flex max-sm:flex-wrap'>
                 <label htmlFor='password ' className='mr-4 text-black font-bold' >Password:</label>
-                <input type='password' name='password' id='password' className='rounded-md max-sm:w-[7rem]' onChange={handler2}></input>
+                <input type='password' name='password' id='password' className='rounded-md max-sm:w-[7rem] bg-white' onChange={handler2}></input>
             </div>
             
             <button className='text-white bg-blue-950 p-2 m-4 rounded-md' onClick={handler3}>login</button>
